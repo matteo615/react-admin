c120e3e9063df2864f38316d7e3bb415f4dbae10
Matt McKaig <worker911@gmail.com>
2023-02-08 00:23:33 -0600

    Tues Pause

--- src/theme.js
+++ src/theme.js
@@ -199,7 +199,7 @@ export const ColorModeContext = createContext({
 
 export const useMode = () => {
     const [mode, setMode] = useState("dark");
-    const colorMode = useMode(
+    const colorMode = useMemo(
         () => ({
             toggleColorMode: () => 
                 setMode((prev) => (prev === "light" ? "dark" : "light"))
